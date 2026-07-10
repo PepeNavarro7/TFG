3b2884fd8c2c86e4575e9dd9c8d679e0b7b33dd7
Pepe <navarrocuartero7@gmail.com>
2026-07-10 13:32:29 +0200

    Separo la parte shuffle del brusselator2d

---
+++ Solver CUDA/Headers/brusselator2d_shuffle.h
@@ -0,0 +1,56 @@
+#ifndef BRUSSELATOR2D_SHUFFLE_H
+#define BRUSSELATOR2D_SHUFFLE_H
+
+#include "Problema.h"
+#include <string>
+
+using namespace std;
+
+
+// Problema 4
+struct Params_brusselator2d_shuffle {
+    int neqn;
+    int nx;
+    double A;
+    double B;
+    double dtx;
+    double DD;
+};
+
+// the Brusselator 2D model 
+class brusselator2d_shuffle: public Problema {
+private:
+    const double alpha = 0.002, A = 1.0, B = 3.4; // variables para el calculo de los valores
+    const int nx; // number of grid points at x dimension
+    const int ny; // number of grid points at y dimension
+    const double dtx_sq; // Spatial step squared
+    const double DD;
+
+public:
+    // Constructor of the class 
+    brusselator2d_shuffle(const int &nx_points, const int &threads):
+        Problema(2.0*nx_points*nx_points, "Brusselator_2D_shuffle", 1.0/(nx_points+1), dim3( (nx_points*nx_points*2.0+threads-1)/threads, 1, 1 ), dim3(threads,1,1)),
+        nx(nx_points), ny(nx_points), dtx_sq(dtx*dtx), DD(alpha/(dtx*dtx)) { };
+
+    // Definicion de los valores constantes para el kernel
+    void updateConstants() const override;
+
+    // Initialize stage vector Y0 with neqn components
+    void init(double* Y0) const override;
+
+    //vector system function for the nonstiff term DY=G(t,Y) + the nonstiff term DY=F(t,Y)
+    void feval (const double &t, const double* Y, double* DY) const override;
+    void feval (const double &h, const double *Y, double* DY, cudaStream_t stream) const override;
+
+    // Exportar los datos a un archivo txt
+    inline void archivo(const string &filename, const double *Y) const override { archivo3(filename,Y); };
+
+private:
+    // Auxiliary function f
+    //double f(const int &i, const int &j, const double &t) const;
+
+    // Indexation function which maps 2D spatial coordinates (i,j) to a 1D position in a vector
+    inline int idx(const int &i, const int &j, const int &k) const { return 2 * (i * ny + j) + k; } 
+};
+
+#endif
---
+++ Solver CUDA/Source/brusselator2d_shuffle.cu
@@ -0,0 +1,91 @@
+#ifndef BRUSSELATOR2D_SHUFFLE_CU
+#define BRUSSELATOR2D_SHUFFLE_CU
+
+#include "brusselator2d_shuffle.h"
+#include <cmath>
+
+using namespace std;
+
+// Problema 4
+
+// Variable en memoria constante (vive en la GPU)
+__constant__ Params_brusselator2d_shuffle cte4_sh;
+
+// Definicion de los valores constantes para el kernel
+void brusselator2d_shuffle::updateConstants() const {
+    Params_brusselator2d_shuffle aux;
+
+    aux.neqn = this->neqn;
+    aux.nx = this->nx;
+    aux.A = this->A;
+    aux.B = this->B;
+    aux.dtx = this->dtx;
+    aux.DD = this->DD;
+
+    cudaMemcpyToSymbol(cte4_sh, &aux, sizeof(Params_brusselator2d_shuffle));
+}
+
+void brusselator2d_shuffle::init(double* Y0) const {
+    for (int i = 0; i < nx; i++) {
+        for (int j = 0; j < ny; j++) {
+            double x_i = (double)(i + 1) * dtx;
+            double y_i = (double)(j + 1) * dtx;
+            Y0[idx(i, j, 0)] = 22 * y_i * pow(1 - y_i, 1.5);
+            Y0[idx(i, j, 1)] = 27 * x_i * pow(1 - x_i, 1.5);
+        }
+    }
+}
+
+// Version del Kernel en la que usamos shuffle
+__global__ void kernel_brusselator2d_shuffle(const double t, const double* __restrict__ Y, double* __restrict__ DY) {
+    const int thread = blockDim.x * blockIdx.x + threadIdx.x, // thread == id_x * 2 * nx + id_y * 2 + id_z  
+              tam_fila = cte4_sh.nx*2, 
+              ult = cte4_sh.nx-1;
+    const int id_x = thread / tam_fila, // Identificamos coordenada x
+              id_y = (thread - id_x*tam_fila) >> 1, // Coordenada Y -> / 2
+              id_z = threadIdx.x & 1, // Coordenada z -> %2
+              lane = threadIdx.x & 31; // Posicion en el warp -> th%32
+
+    if(thread < cte4_sh.neqn){
+        // Calculamos los indices y valores de los vecinos verticales
+        const int i_arriba  =  id_x==0  ? (ult*tam_fila  + id_y*2 + id_z) : (thread-tam_fila),
+                  i_abajo   = id_x==ult ? (     0        + id_y*2 + id_z) : (thread+tam_fila);
+        const double valor = Y[thread], val_arriba=Y[i_arriba], val_abajo=Y[i_abajo];
+        const unsigned mask = 0xFFFFFFFF;
+
+        // Calculamos los shuffles para los vecinos horizontales
+        double val_izq  = __shfl_up_sync  (mask, valor, 2), // th(n-2) -> th(n)
+               val_der  = __shfl_down_sync(mask, valor, 2), // th(n) <- th(n+2) 
+               par_up   = __shfl_up_sync  (mask, valor, 1), // th(n-1) -> th(n)
+               par_down = __shfl_down_sync(mask, valor, 1); // th(n) <- th(n+1) 
+
+        // Reescribimos los shuffles que no son correctos
+        if(lane<=1 || id_y==0) // lanes 0 y 1 shuffle incorrecto, id_y==0 es frontera horizontal
+            val_izq = id_y==0   ? Y[id_x*tam_fila +  ult*2 + id_z] : Y[thread-2];
+        if(lane>=30 || id_y==ult) // lanes 30 y 31 shuffle incorrecto, id_y==ult es frontera horizontal
+            val_der = id_y==ult ? Y[id_x*tam_fila +    0   + id_z] : Y[thread+2];
+        const double val_pareja = id_z==0 ? par_down : par_up; // Nos quedamos el valor pareja apropiado
+
+        const double u_ij = id_z==0 ? valor : val_pareja,
+                     v_ij = id_z==1 ? valor : val_pareja;
+        const double term = cte4_sh.B * u_ij - u_ij * u_ij * v_ij;
+
+        DY[thread] = cte4_sh.DD * (val_arriba + val_izq - 4.0 * valor + val_abajo + val_der);
+        
+        const double x = (id_x + 1) * cte4_sh.dtx, y = (id_y + 1) * cte4_sh.dtx;
+        const double xmxc = x - 0.3, ymyc = y - 0.5;
+        const double r = 0.1;
+        const double result = ((xmxc * xmxc + ymyc * ymyc) <= r * r && t >= 1.1) ? 5.0 : 0.0;
+
+        DY[thread] += id_z==0 ? (cte4_sh.A - term - u_ij + result) : term;
+    }
+}
+
+void brusselator2d_shuffle::feval(const double &t, const double* Y, double* DY) const {
+    kernel_brusselator2d_shuffle<<<this->grid,this->block>>>(t, Y, DY);
+}
+void brusselator2d_shuffle::feval (const double &h, const double *Y, double* DY, cudaStream_t stream) const {
+    kernel_brusselator2d_shuffle<<<this->grid, this->block, 0, stream>>>(0.0, Y, DY);
+}
+
+#endif
